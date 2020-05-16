91069a627beecdb7d4a64e2837d006e7b06aa5fa
Nicholas Rutherford <nicholasrutherford@Nicholass-MacBook-Air.local>
2020-05-15 22:42:00 -0500

    created challenge data class

---
+++ code/challenger.api/src/main/kotlin/com/nickrutherford/challenger/api/models/Challenge.kt
@@ -0,0 +1,17 @@
+package com.nickrutherford.challenger.api.models
+
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+import javax.validation.constraints.NotBlank
+
+@Entity(name = "challenge")
+data class Challenge(
+        @Id @GeneratedValue(
+                strategy = GenerationType.IDENTITY) val id: Long = 0,
+        @get: NotBlank val name: String = "",
+        @get: NotBlank val desc: String = "",
+        @get: NotBlank val category: String = "",
+        @get: NotBlank val imageUrl: String = "") {
+}
